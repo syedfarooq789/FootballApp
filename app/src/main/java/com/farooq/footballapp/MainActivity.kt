04b48f6cb11c27d93427fd554d3735064c72fd0b
Farooq <farooq.hassan@m-files.com>
2023-12-28 16:18:45 +0200

    Android Football App: Added main activity

--- app/src/main/java/com/farooq/footballapp/MainActivity.kt
+++ app/src/main/java/com/farooq/footballapp/MainActivity.kt
@@ -3,44 +3,38 @@ package com.farooq.footballapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import com.farooq.footballapp.data.util.NetworkMonitor
+import com.farooq.footballapp.ui.FootballApp
 import com.farooq.footballapp.ui.theme.FootballAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+
+
+/**
+ * Created by Syed Farooq Hassan on 12/27/2023.
+ *
+ * Main activity containing the main football app.
+ */
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FootballAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                FootballApp(
+                    networkMonitor = networkMonitor,
+                    windowSizeClass = calculateWindowSizeClass(this)
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FootballAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/farooq/footballapp/MainActivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.farooq.footballapp
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * Created by Syed Farooq Hassan on 12/25/2023.
+ */
+class MainActivityViewModel : ViewModel() {
+
+}
