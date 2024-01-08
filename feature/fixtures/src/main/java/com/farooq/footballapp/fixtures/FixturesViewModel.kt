5b1168a042a17ebf74510dc91cf4381b0be84dbc
Farooq <farooq.hassan@m-files.com>
2024-01-08 16:03:47 +0200

    Android Football App: Added fixtures view model

---
+++ feature/fixtures/src/main/java/com/farooq/footballapp/fixtures/FixturesViewModel.kt
@@ -0,0 +1,13 @@
+package com.farooq.footballapp.fixtures
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+/**
+ * Created by Syed Farooq Hassan on 12/29/2023.
+ */
+@HiltViewModel
+class FixturesViewModel @Inject constructor() : ViewModel() {
+}
