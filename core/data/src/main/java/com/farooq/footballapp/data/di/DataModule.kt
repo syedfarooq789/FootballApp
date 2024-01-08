d27b0bffbe0e6f1142a689b13f92996e6f90595f
Farooq <farooq.hassan@m-files.com>
2024-01-08 16:01:57 +0200

    Android Football App: Binds the network monitor with its implementation

---
+++ core/data/src/main/java/com/farooq/footballapp/data/di/DataModule.kt
@@ -0,0 +1,21 @@
+package com.farooq.footballapp.data.di
+
+import com.farooq.footballapp.data.util.ConnectivityManagerNetworkMonitor
+import com.farooq.footballapp.data.util.NetworkMonitor
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+/**
+ * Created by Syed Farooq Hassan on 1/4/2024.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+    @Binds
+    internal abstract fun bindsNetworkMonitor(
+        networkMonitor: ConnectivityManagerNetworkMonitor,
+    ): NetworkMonitor
+}
